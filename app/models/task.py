f54e01b8b7643e5d7b29bbd972c812974e6a1d66
francescafr <hello@francesca.fyi>
2022-11-07 23:26:29 -0800

    Completed Wave 1

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,25 @@
 from app import db
 
-
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20))
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+    
+    #def check_complete(self):
+        # need to figure out how to reference in other functions
+        #can I do that without including in parameters?
+        
+
+    def to_dict(self):
+        if self.completed_at != None:
+            check_status = True
+        else:
+            check_status = False
+
+        return dict(
+                    id=self.task_id,
+                    title=self.title,
+                    description=self.description,
+                    is_completed=check_status
+        )
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,66 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, make_response, abort
+from app import db
+from app.models.task import Task
+from app.models.goal import Goal
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+goal_bp = Blueprint("goals", __name__, url_prefix="/goals")
+
+def validate_task(id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({"message": f"Task {id} is invalid"}, 400))
+
+    task = Task.query.get(id)
+    if not task:
+        abort(make_response({"message": f"Task {id} not found"}, 404))
+    return task
+
+
+def validate_new_task(request_body):
+    try:
+        new_task = Task(title=request_body["title"],
+                        description=request_body["description"],
+                        completed_at=request_body["completed_at"])
+    except:
+        abort(make_response({"details": "Invalid data"},400))
+    return new_task
+
+
+@task_bp.route("", methods =["GET","POST"])
+def handle_tasks():
+    if request.method == 'GET':
+        tasks = Task.query.all()
+        tasks_response = [task.to_dict() for task in tasks]
+        return make_response(jsonify(tasks_response), 200)
+
+    if request.method == 'POST':
+        request_body = request.get_json()
+        new_task = validate_new_task(request_body)
+        db.session.add(new_task)
+        db.session.commit()
+        return(make_response("New Task Created!", 201))
+
+
+@task_bp.route("/<task_id>", methods=["GET","PUT","DELETE"])
+def one_task(task_id):
+    task = validate_task(task_id)
+    if request.method == 'GET':
+        return make_response(task.to_dict(), 200)
+    
+    if request.method == 'PUT':
+        request_body = validate_new_task(request.get_json())
+
+        task.title=request_body["title"]
+        task.description=request_body["description"]
+        task.completed_at=request_body["completed_at"]
+
+        db.session.commit()
+        return(make_response(f"Task #{task.task_id} successfully updated", 200))
+
+    if request.method == 'DELETE':
+        db.session.delete(task)
+        db.session.commit()
+
+        return make_response(f"Task #{task.task_id} successfully deleted",200)
